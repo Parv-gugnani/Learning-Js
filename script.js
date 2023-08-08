8871ae38d0b5d62dc1fba6de76c88677fe693dd6
Parv-gugnani <9765.parv@gmail.com>
2023-08-08 17:07:45 +0530

    Just Starting!

---
+++ script.js
@@ -0,0 +1,10 @@
+let message = "Hello world!";
+const pi = 3.14159; //float //cant be changed it is truth that why we store in constants
+let name = "John"; //string
+let age = 30; //int
+let isStudent = true; //boolean
+let fruits = ["apple", "banana", "orange"]; //array
+let person = { name: "John", age: 30, isStudent: true }; //object
+
+console.log(message);
+//output of message var
