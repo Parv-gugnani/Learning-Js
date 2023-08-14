d8d9c705ff5c5c9ccec8f80e3dcb268f330c7892
Parv-gugnani <9765.parv@gmail.com>
2023-08-14 23:29:34 +0530

    4

---
+++ problem.js
@@ -0,0 +1,35 @@
+/*
+Declare a variable called 'message' and assign the string "Hello, world!" to it. 
+Then, use 'console.log' to print the value of 'message' to the console.
+*/
+var mus = "Hello, world!";
+
+console.log(mus);
+
+/*
+Create two variables: 'num1' with a value of 5 and 'num2' with a value of 3. 
+Calculate and print the sum of these two numbers using 'console.log'.
+*/
+
+num1 = 5;
+num2 = 3;
+
+console.log(num1 + num2);
+
+/*
+Write a program that checks if a given number is greater than 10. 
+If it is, print "Number is greater than 10." Otherwise, print "Number is not greater than 10."
+ */
+
+let givenNumber = 15;
+
+if (givenNumber > 10) {
+  console.log("Number is greater than 10");
+} else {
+  console.log("Number is not greater than 10");
+}
+
+/*Using a loop, print the numbers from 1 to 5 in the console. */
+for (i = 1; i < 6; i++) {
+  console.log("this is counting :", i);
+}
