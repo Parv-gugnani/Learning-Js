31d66af41b3d4e557eef9ed32ead9bf75a628e55
Parv-gugnani <9765.parv@gmail.com>
2023-08-17 18:50:40 +0530

    10 DONE

--- problem.js
+++ problem.js
@@ -45,3 +45,61 @@ function greet(name) {
 
 let greeting = greet("Parv");
 console.log(greeting);
+
+/*Create an array called 'colors' with three strings: 
+"red", "green", and "blue". Use a loop to print each color in the array.
+ */
+
+let colors = ["red", "green", "blue"];
+//only mistake i did is used the method of c programing didt used .length
+for (i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+/*Write a function called 'calculateSquare' 
+that takes a number as a parameter and returns the square of that number.
+*/
+
+function calculateSquare(num) {
+  return num * num;
+}
+console.log(calculateSquare(5));
+
+/* Create a program that checks if a given number is positive, negative, or zero. 
+Print the appropriate message based on the condition.
+*/
+
+let number = 7;
+
+if (number < 0) {
+  console.log("Negative");
+} else if ((number = 0)) {
+  console.log("Equal to zero");
+} else {
+  console.log("Positive");
+}
+
+/**Create an object called 'person' with properties 'name', 'age', and 'gender'.
+ * Assign values of your choice to each property.
+ */
+
+let person = {
+  name: "John",
+  age: 30,
+  gender: "male",
+};
+
+/** Write a function called 'getInitials' that takes a full name as a parameter and
+ * returns the initials in uppercase.
+ */
+
+function getInitials(fullname) {
+  let name = fullName.split(" ");
+  let initials = "";
+  for (let i = 0; i < name.length; i++) {
+    initials += name[i][0].toUpperCase();
+  }
+  return initials;
+}
+
+console.log(initials);
